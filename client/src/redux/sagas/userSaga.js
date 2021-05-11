9cd427b907d0e06524c90e6f7524f1c3ad31b873
vjvl95 <vjvl95@naver.com>
2021-05-12 08:25:39 +0900

    0512

--- client/src/redux/sagas/userSaga.js
+++ client/src/redux/sagas/userSaga.js
@@ -10,13 +10,11 @@ const loadUserAPI = (payload) =>{
             "X-AUTH-TOKEN": localStorage.getItem("token")
         },
     }
-    console.log(localStorage.getItem("token"))
     return axios.get("/api/v1/account",payload,config);
 }
 
 function* loadUser(action){
     try{
-
         const result= yield call(loadUserAPI,action.payload)
         console.log(result, "loadUser");
 
@@ -31,9 +29,7 @@ function* loadUser(action){
         })
     }
 }
-function* watchLoadUser(){
-    yield takeEvery(USER_LOADING_REQUEST, loadUser)
-}
+
 
 const loadUserDetailAPI = (payload) =>{
     const config = {
@@ -63,17 +59,88 @@ function* loadUserDetail(action){
     }
     
 }
+
+
+const UserDeleteAPI = (payload) =>{
+    return axios.delete("/api/v1/account",{ headers:{"X-AUTH-TOKEN":localStorage.getItem("token"),"Content-Type" : "application/json"} , data:{idString: payload.idString}});
+}
+
+
+function* UserDelete(action){
+    try{
+        const result= yield call(UserDeleteAPI,action.payload)
+        console.log(result, "UserDelete");
+
+        yield put({
+            type:USER_DELETE_SUCCESS,
+        })
+        alert(action.payload.name+"님이 삭제되었습니다.")
+        yield put(push("/admin/user"))
+    } catch(e){
+        yield put({
+            type: USER_DELETE_FAILURE,
+            payload:e
+        })
+    }    
+}
+
+
+const UserSearchAPI = (payload) =>{
+    const config = {
+        headers:{
+            "Content-Type" : "application/json",
+            "X-AUTH-TOKEN": localStorage.getItem("token")
+        },
+    }
+
+    console.log(payload.pageRequest.direction)
+    console.log(payload.pageRequest.page)
+    console.log(payload.pageRequest.size)
+
+
+    return axios.get(`/api/v1/account/search?pageRequest.direction=${payload.pageRequest.direction}&pageRequest.page=${payload.pageRequest.page}&pageRequest.size=${payload.pageRequest.size}&type=${payload.pageRequest.type}`,config);
+}
+
+
+function* UserSearch(action){
+    try{
+        const result= yield call(UserSearchAPI,action.payload)
+        console.log(result, "UserSearch");
+
+        yield put({
+            type:USER_SEARCH_SUCCESS,
+        })
+        alert(action.payload.name+"님이 삭제되었습니다.")
+        yield put(push("/admin/user"))
+    } catch(e){
+        yield put({
+            type: USER_SEARCH_FAILURE,
+            payload:e
+        })
+    }    
+}
+
+function* watchUserDelete(){
+    yield takeEvery(USER_DELETE_REQUEST, UserDelete)
+}
+
+function* watchLoadUser(){
+    yield takeEvery(USER_LOADING_REQUEST, loadUser)
+}
+
 function* watchLoadUserDetail(){
     yield takeEvery(USER_DETAIL_REQUEST, loadUserDetail)
 }
 
-
+function* watchLoadUserSearch(){
+    yield takeEvery(USER_SEARCH_REQUEST, UserSearch)
+}
 
 
 
 export default function* userSaga() {
       
     yield all([
-       fork(watchLoadUser), fork(watchLoadUserDetail)
+       fork(watchLoadUser), fork(watchLoadUserDetail), fork(watchUserDelete),fork(watchLoadUserSearch)
     ]);
   }
